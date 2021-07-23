e7e5da3b0f7e4cf6066c1e476223fb1ca8993734
Jaco van den Bergh <jacovdbergh@gmail.com>
2021-07-23 14:37:05 +0200

    Update menu.blade.php

--- source/_nav/menu.blade.php
+++ source/_nav/menu.blade.php
@@ -1,16 +1,16 @@
 <nav class="items-center justify-end hidden text-lg lg:flex">
     <a title="{{ $page->siteName }} Blog" href="/blog"
-        class="ml-6 text-green-700 hover:text-green-600 {{ $page->isActive('/blog') ? 'active text-green-600' : '' }}">
+        class="ml-6 {{ $page->isActive('/blog') ? 'active text-green-600' : 'text-green-700' }} hover:text-green-600">
         Blog
     </a>
 
     <a title="{{ $page->siteName }} About" href="/about"
-        class="ml-6 text-green-700 hover:text-green-600 {{ $page->isActive('/about') ? 'active text-green-600' : '' }}">
+        class="ml-6 {{ $page->isActive('/about') ? 'active text-green-600' : 'text-green-700' }} hover:text-green-600">
         About
     </a>
 {{-- 
     <a title="{{ $page->siteName }} Contact" href="/contact"
-        class="ml-6 text-green-700 hover:text-green-600 {{ $page->isActive('/contact') ? 'active text-green-600' : '' }}">
+        class="ml-6 {{ $page->isActive('/contact') ? 'active text-green-600' : 'text-green-700' }} hover:text-green-600">
         Contact
     </a> --}}
 </nav>
