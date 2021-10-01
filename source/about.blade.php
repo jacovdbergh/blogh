0a6c00623296831ca1c49d055076971c3b45991d
Jaco van den Bergh <jacovdbergh@gmail.com>
2021-10-01 08:51:24 +0200

    About

--- source/about.blade.php
+++ source/about.blade.php
@@ -5,14 +5,14 @@ description: A little bit about the site
 @extends('_layouts.master')
 
 @section('body')
-    <h1>About Jaco</h1>
+    <h1>About Jaco van den Bergh</h1>
     <p class="-mt-4 text-sm text-green-600">husband&nbsp;&nbsp;•&nbsp;&nbsp;father&nbsp;&nbsp;•&nbsp;&nbsp;computer engineer</p>
 
     <img src="/assets/img/headshot.png"
         alt="About image"
         class="flex w-64 h-64 mx-auto my-2 bg-contain rounded-full md:float-right md:ml-10">
 
-    <p class="mb-6">My name's Jaco van den Bergh. By day, I do software development for a ticketing company in South Africa. In my free time I like to build and hack on things that makes me excited or my life easier.</p>
+    <p class="mb-6">By day, I do software development for a ticketing company in South Africa. In my free time I like to build and hack on things that excite me or make my life easier.</p>
 
     <p class="mb-6">You can follow me on Twitter, <a target="_blank" href="https://twitter.com/jacovdbergh">@jacovdbergh</a>, or see what I'm up to on Instagram, also <a target="_blank" href="https://instagram.com/jacovdbergh">@jacovdbergh</a></p>
 
